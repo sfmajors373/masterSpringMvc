05a4c874a00b2859b40dd75621afbe9cc1d9ce21
sfmajors373 <sfmajors373@gmail.com>
2019-03-06 07:43:19 -0500

    Generated with curl start.Spring.io

---
+++ src/main/java/masterspringmvc/masterspringmvc/MasterspringmvcApplication.java
@@ -0,0 +1,13 @@
+package masterspringmvc.masterspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterspringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterspringmvcApplication.class, args);
+	}
+
+}
